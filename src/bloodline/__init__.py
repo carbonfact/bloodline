6b7251cdeeb5603cbd5771305ddbe984b8ce463c
Alexis Cruveiller <alexis@carbonfact.com>
2025-10-14 11:33:02 +0200

    Rename integration folder to accessors

--- src/bloodline/__init__.py
+++ src/bloodline/__init__.py
@@ -1,6 +1,6 @@
+from .accessors.pandas import LineageAccessor  # noqa: F401
 from .core import get_source_type, register_source_type, update_data_lineage, update_table_data_lineage
-from .integration.pandas import LineageAccessor  # noqa: F401
-from .source import Source
+from .source import Source, set_source_registry
 
 __all__ = [
     "Source",
@@ -8,4 +8,5 @@ __all__ = [
     "register_source_type",
     "update_data_lineage",
     "update_table_data_lineage",
+    "set_source_registry"
 ]
