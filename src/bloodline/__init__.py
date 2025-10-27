0fa86437c040068d3886b63eb57b0a1f2fe552ec
Max Halford <maxhalford25@gmail.com>
2025-10-27 18:26:25 +0100

    ER diagram proposal (#2)
    One of the goals for this package is to generate an ER diagram. I think
    it's interesting to start thinking about this now, because it will
    affect the rest of the package.
    
    I've added an example in `test_er_diagram.ipynb`. I added some TODOs
    that we can discuss.

--- src/bloodline/__init__.py
+++ src/bloodline/__init__.py
@@ -1,12 +1,17 @@
-from .accessors.pandas import LineageAccessor  # noqa: F401
+from .accessors.pandas import (
+    RELATIONSHIPS,
+    LineageAccessor,  # noqa: F401
+)
 from .core import get_source_type, register_source_type, update_data_lineage, update_table_data_lineage
 from .source import Source, set_source_registry
 
 __all__ = [
     "Source",
+    "LineageAccessor",
     "get_source_type",
     "register_source_type",
     "update_data_lineage",
     "update_table_data_lineage",
-    "set_source_registry"
+    "set_source_registry",
+    "RELATIONSHIPS",
 ]
